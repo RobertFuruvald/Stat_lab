7b7a9a31e31049640af79a3e7d6d2d5a0b78264a
Robert Furuvald <rfu@kth.se>
2024-09-06 20:17:13 +0200

    Task 5 and figures

--- Project 1/calculations.py
+++ Project 1/calculations.py
@@ -2,12 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-# probability functions for each dice
+# Probability functions for each dice
 def p_dice(n):
     return np.ones(n) / n
 
 
-# Create the different dices
+# Create the different dice probabilities of each outcome
 tetra_pf = p_dice(4)
 cube_pf = p_dice(6)
 octa_pf = p_dice(8)
@@ -24,14 +24,13 @@ S = np.convolve(S, icosa_pf)
 P_winning = sum(S[0:6]) + sum(S[40:])
 print("Probability of winning: ", P_winning)
 
-# Monte-Carlo
-trials = [pow(10, i) for i in range(1, 6)]
 
-
+# Method to create a random number for a given dice
 def rand_dice(n):
     return np.random.randint(1, n + 1)
 
 
+# Monte-Carlo method for number of trials
 def monte(trial):
     win = 0
     for _ in range(trial):
@@ -42,16 +41,17 @@ def monte(trial):
     return win / trial
 
 
-# task 3 1000 trials with the monte method
+# Task 3 1000 trials with the monte method
 print("Task 3 1000 trials with the monte method: ", monte(1000))
 
-# task 4 run different number of trials
+# Task 4 run different number of trials
+trials = [pow(10, i) for i in range(1, 6)]
 task_4 = []
 for trial in trials:
     task_4.append(monte(trial))
 
 
-# task 5
+# Task 5
 
 # Function to simulate the Monte Carlo relative error
 def monte_trials(n, max_trials=100000):
@@ -63,14 +63,13 @@ def monte_trials(n, max_trials=100000):
         end_result.append(relative_error)
         n_trials.append(n)
         n += 1000
-    # Returns the number of trials
+    # Returns the number of trials and the relative error of each trial
     return n_trials, end_result
 
 
 task_5 = monte_trials(1000)
 
-
-# Plots for the tasks
+# Plots for the different tasks
 # The sum S ranges from 5 to 50
 possible_sums = np.arange(5, 51)
 # data prep
@@ -80,14 +79,14 @@ table_data = [[s, f"{prob:.6f}"] for s, prob in zip(possible_sums, S)]
 fig, ax = plt.subplots(figsize=(8, 8))
 ax.axis('off')  # Turn off the axis
 
-# Create the table
+# Create the table for task 1
 table = ax.table(cellText=table_data, colLabels=["S", "P(S=s)"], cellLoc='center', loc='center')
 
 # Adjust the table's style
 table.auto_set_font_size(False)
 table.set_fontsize(10)
 
-# Create a bar chart
+# Create a bar chart for task 1
 fig, ax = plt.subplots(figsize=(8, 8))
 ax.bar(possible_sums, S, color='skyblue', edgecolor='black')
 
@@ -101,7 +100,7 @@ fig, ax = plt.subplots(figsize=(8, 8))
 ax.plot(trials, task_4, marker='o', linestyle='-', color='blue', label='Probability')
 ax.axhline(y=P_winning, color='red', linestyle='--', label='Exact Probability')
 
-# Label
+# Label and title
 ax.set_xlabel('Number of Trials')
 ax.set_ylabel('Estimated Probability')
 ax.set_title('Estimated Probability for n trials')
